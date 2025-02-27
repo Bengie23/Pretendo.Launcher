cdbcfa3fd698be8fb1da2a0dbe4636465e916497
Ben Lopez <benswengineer@outlook.com>
2025-02-26 22:55:21 -0500

    add backend launcher for nginx

---
+++ backend.with.nginx.launcher.ps1
@@ -0,0 +1,76 @@
+#Requires -RunAsAdministrator
+
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+Write-Output "This script needs to be run As Admin"
+Break
+}
+
+function Test-Backend {
+    if (Test-Path -Path .\backend) {
+        return $true;
+    }
+    return $false;
+}
+
+if (-Not (Test-Backend)){
+    Write-Host "Downloading Backend..."
+    Invoke-WebRequest 'https://github.com/Bengie23/Pretendo.Backend/archive/refs/heads/master.zip' -OutFile .\backend.zip
+    Expand-Archive .\backend.zip .\ 
+    Rename-Item .\Pretendo.Backend-master .\backend
+    Remove-Item .\backend.zip
+}
+
+Set-Location .\backend
+Write-Host "Running Backend..."
+dotnet run --configureation Release --property WarningLevel=0 #Requires -RunAsAdministrator
+
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+Write-Output "This script needs to be run As Admin"
+Break
+}
+
+function Test-Backend {
+    if (Test-Path -Path .\backend) {
+        return $true;
+    }
+    return $false;
+}
+
+if (-Not (Test-Backend)){
+    Write-Host "Downloading Backend..."
+    Invoke-WebRequest 'https://github.com/Bengie23/Pretendo.Backend/archive/refs/heads/master.zip' -OutFile .\backend.zip
+    Expand-Archive .\backend.zip .\ 
+    Rename-Item .\Pretendo.Backend-master .\backend
+    Remove-Item .\backend.zip
+}
+
+Set-Location .\backend
+Write-Host "Running Backend..."
+dotnet run --configureation Release --property WarningLevel=0 #Requires -RunAsAdministrator
+
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+Write-Output "This script needs to be run As Admin"
+Break
+}
+
+function Test-Backend {
+    if (Test-Path -Path .\backend) {
+        return $true;
+    }
+    return $false;
+}
+
+if (-Not (Test-Backend)){
+    Write-Host "Downloading Backend..."
+    Invoke-WebRequest 'https://github.com/Bengie23/Pretendo.Backend/archive/refs/heads/master.zip' -OutFile .\backend.zip
+    Expand-Archive .\backend.zip .\ 
+    Rename-Item .\Pretendo.Backend-master .\backend
+    Remove-Item .\backend.zip
+}
+
+Set-Location .\backend
+Write-Host "Running Backend..."
+dotnet run --configureation Release --property WarningLevel=0  --launch-profile "nginx"
